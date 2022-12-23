744c0e6fd28f3063ccced6b38026684889e9c11d
Ivan Beketov <almighty2806@gmail.com>
2022-12-23 14:57:44 +0700

    Приветствие

---
+++ lab7.js
@@ -0,0 +1,27 @@
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    let userSex = document.getElementById('sex').value;
+
+    let greeting;
+    if(userAge <= 17) {
+        greeting = 'Здравствуй, ';
+        if(userSex == 'female') {
+            greeting += 'девочка ';
+        }
+        else {
+            greeting += 'мальчик ';
+        }
+    }
+    else {
+        greeting = 'Здравствуйте, ';
+        if(userSex == 'female') {
+            greeting += 'госпожа ';
+        }
+        else {
+            greeting += 'господин ';
+        }
+    }
+    alert(greeting + userName + ' ' + userSurname + '\n ' + 'Ваш возраст: ' + userAge);
+}
