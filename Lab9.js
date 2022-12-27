960d37d2865ba2ef3093d2884ddf4c100eaa8678
Ivan Beketov <almighty2806@gmail.com>
2022-12-27 10:33:32 +0700

    Кнопка Имя-Фамилия

--- Lab9.js
+++ Lab9.js
@@ -4,3 +4,15 @@ function clickMe(button) {
     else
     button.innerHTML = 'Щелчок'
 }
+function student1(button) {
+    if(button.innerHTML == 'Давыдов')
+        button.innerHTML = 'Дмитрий';
+    else
+    button.innerHTML = 'Давыдов'
+}
+function student2(button) {
+    if(button.innerHTML == 'Бекетов')
+        button.innerHTML = 'Иван';
+    else
+    button.innerHTML = 'Бекетов'
+}
