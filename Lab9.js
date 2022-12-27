91ca3970ddfc6d1d0fa2b331a9ee33b3dd46dea1
Ivan Beketov <almighty2806@gmail.com>
2022-12-27 10:43:32 +0700

    Вывод координат мыши

--- Lab9.js
+++ Lab9.js
@@ -37,3 +37,10 @@ function knopka(button) {
         button.style.color = 'gray'; 
     }
 }
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ' ' + ', y = ' + event.offsetY + ' ' + (event.ctrlKey? '<b>Ctrl</b>':'<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>':'<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>':'<s>Alt</s>' );
+}
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove',  showCoords);
+});
