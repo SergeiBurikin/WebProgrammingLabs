5aa666403e27f8e7a4bcc5b20a61e24344f4c547
Ivan Beketov <almighty2806@gmail.com>
2022-12-27 10:38:02 +0700

    Переключение состояний

--- Lab9.js
+++ Lab9.js
@@ -16,3 +16,24 @@ function student2(button) {
     else
     button.innerHTML = 'Бекетов'
 }
+function knopka(button) {
+    if(button.innerHTML == 'Нажми меня') {
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'red';
+    }
+        
+    else if(button.innerHTML == 'Ещё раз нажми') {
+        button.innerHTML = 'Ещё';  
+        button.style.color = 'green';
+    }
+         
+    else if(button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';   
+        button.style.color = 'blue';
+    }
+        
+    else {
+        button.innerHTML = 'Нажми меня' 
+        button.style.color = 'gray'; 
+    }
+}
